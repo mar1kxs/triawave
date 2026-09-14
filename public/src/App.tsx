dcea7764ab115e68ae9118f0ca305d2b1fcb00d3
mar1kxs <maksim234.kyky@gmail.com>
2026-09-14 21:02:02 +0200

    fix

--- public/src/App.tsx
+++ public/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import Home from './pages/Home/Home'
 import Header from './components/Header/Header'
 
--- public/src/components/Header/Header.tsx
+++ public/src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import './Header.css'
 import logo from '../../assets/images/logo_header.svg'
 
